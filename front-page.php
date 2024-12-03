11ad0b17c3fe5b6e12d34df452ef4840ad725a8d
eunjin6-6 <dmswls616@gmail.com>
2024-12-03 09:59:14 +0900

    front-page 추가

---
+++ front-page.php
@@ -0,0 +1,38 @@
+<?php get_header() ?>
+
+  <div id="content" class="two-thirds">
+   
+  <h2>Lastest Works</h2>
+
+  <?php
+        query_posts( array(
+          'category_name'  => 'works',
+          'posts_per_page' => 3
+        ) );
+      ?>
+      <ul>
+        <?php
+          // The Loop
+          while ( have_posts() ) : the_post();
+        ?>
+        <li>
+
+          <a href="<?php the_permalink(); ?>">
+            <?php
+              if ( has_post_thumbnail() ) {
+                the_post_thumbnail( array( 50, 50 ) );
+              }
+            ?>
+          </a>
+        </li>
+        <?php
+          endwhile;
+        ?>
+      </ul>
+      <?php wp_reset_query(); ?>
+      
+  </div><!-- #content-->      
+
+  <?php get_sidebar() ?>
+
+  <?php get_footer() ?>
